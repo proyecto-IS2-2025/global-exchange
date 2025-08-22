5bacffe039bace769479eb0e2d6ff2754a90d411
Elaivan <ivanfigueredo2808@gmail.com>
2025-08-22 16:28:32 -0300

    Conversaciones con las IAs que se utilizaron

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/ivan/PycharmProjects/global-exchange/.venv
+set -gx VIRTUAL_ENV /home/ivan/proyectoIS2/global-exchange/.venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
