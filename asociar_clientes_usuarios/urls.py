b1dfd397de15c8f69361f0d8c4dff56d0cccadda
leoRcv <jorge06112002@fpuna.edu.py>
2025-08-27 22:21:04 -0300

    estado de asociar clientes terminados

--- asociar_clientes_usuarios/urls.py
+++ asociar_clientes_usuarios/urls.py
@@ -1,7 +1,9 @@
 # asociar_clientes_usuarios/urls.py
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/asociar/', views.asociar_admin_view, name='asociar_admin'),
+    path('asociar_admin/', views.asociar_admin_view, name='asociar_admin'),
+    path('listar_asociaciones/', views.listar_asociaciones, name='listar_asociaciones'),
 ]
