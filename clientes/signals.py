5768ed55b441610b67b0ce6410d831a34f1aa43f
Isaac Gonzalez <gonzalez.isaac.ulises.2001@fpuna.edu.py>
2025-09-18 23:18:56 -0300

    feat(clientes): regenerar cotizaciones al editar Descuento

--- clientes/signals.py
+++ clientes/signals.py
@@ -1,7 +1,7 @@
 # clientes/signals.py
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from clientes.models import Segmento
+from clientes.models import Segmento, Descuento
 from divisas.models import Divisa, TasaCambio
 from divisas.services import generar_cotizaciones_por_segmento
 
@@ -17,9 +17,20 @@ def generar_cotizaciones_para_segmento_nuevo(sender, instance, created, **kwargs
 
     usuario = None  # o asignar un usuario de sistema si corresponde
 
-    # recorrer todas las divisas activas
     for divisa in Divisa.objects.filter(is_active=True):
-        # tomar la última tasa creada para esa divisa
+        tasa = TasaCambio.objects.filter(divisa=divisa).order_by('-fecha').first()
+        if tasa:
+            generar_cotizaciones_por_segmento(divisa, tasa, usuario)
+
+
+@receiver(post_save, sender=Descuento)
+def regenerar_cotizaciones_si_descuento_cambia(sender, instance, created, **kwargs):
+    """
+    Cuando se crea o edita un Descuento, regenerar cotizaciones para el segmento afectado.
+    """
+    usuario = None  # igual que arriba
+
+    for divisa in Divisa.objects.filter(is_active=True):
         tasa = TasaCambio.objects.filter(divisa=divisa).order_by('-fecha').first()
         if tasa:
             generar_cotizaciones_por_segmento(divisa, tasa, usuario)
