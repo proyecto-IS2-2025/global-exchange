4062e6573cf4be088fc9e330f4812bd25eb8becf
NicoarxPy <nico.arza.2002@gmail.com>
2025-10-10 19:04:32 -0300

    Tansferencia automática implementada

--- transacciones/models.py
+++ transacciones/models.py
@@ -30,6 +30,7 @@ class Transaccion(models.Model):
     ESTADO_CHOICES = [
         ('pendiente', 'Pendiente'),
         ('pagada', 'Pagada'),
+        ('completado', 'Completado'),  # <-- NUEVO estado
         ('cancelada', 'Cancelada'),
         ('anulada', 'Anulada'),
     ]
