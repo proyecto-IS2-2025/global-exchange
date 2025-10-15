cbc7ded9f5a4ac348fa417863d679265fdc4acd2
Isaac Gonzalez <gonzalez.isaac.ulises.2001@fpuna.edu.py>
2025-10-14 22:46:43 -0300

    correccion de migraciones y eliminacion de archivos innecesarios

--- start_celery_beat.ps1
+++
@@ -1,3 +0,0 @@
-# Script para iniciar Celery Beat
-Write-Host "Iniciando Celery Beat..." -ForegroundColor Cyan
-poetry run celery -A casa_de_cambios beat --loglevel=info
--- start_celery_worker.ps1
+++
@@ -1,3 +0,0 @@
-# Script para iniciar Celery Worker
-Write-Host "Iniciando Celery Worker..." -ForegroundColor Cyan
-poetry run celery -A casa_de_cambios worker --loglevel=info --pool=solo
