a69cd8ca12689b84457940b557d9998ee64a6a7c
NicoarxPy <nico.arza.2002@gmail.com>
2025-10-24 18:20:44 -0300

    Agregado a settings.py DEFAULT_FROM_EMAIL

--- casa_de_cambios/settings.py
+++ casa_de_cambios/settings.py
@@ -185,7 +185,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'glex.globalexchange.respaldo@gmail.com')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'itlf keib ybar gyds')
-
+DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER', 'glex.globalexchange.respaldo@gmail.com') 
 # ⚠️ RECOMENDACIÓN: Mover credenciales de email a .env
 # EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 # EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
